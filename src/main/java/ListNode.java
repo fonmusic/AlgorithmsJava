fd27c2a8df59864dd4864e1a43f9b8088a03cadf
fonmusic <red-strat@mail.ru>
2023-07-12 16:22:31 +0300

    ListNode class has added

---
+++ src/main/java/ListNode.java
@@ -0,0 +1,50 @@
+public class ListNode {
+    int value;
+    ListNode next;
+
+    ListNode(int value) {
+        this.value = value;
+    }
+
+    public static ListNode reverseLinkedList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+    private static void printLinkedList(ListNode head) {
+        ListNode current = head;
+
+        while (current != null) {
+            System.out.print(current.value + " ");
+            current = current.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        listNode.next = new ListNode(2);
+        listNode.next.next = new ListNode(3);
+        listNode.next.next.next = new ListNode(4);
+        listNode.next.next.next.next = new ListNode(5);
+
+        System.out.println("Исходный список:");
+        printLinkedList(listNode);
+
+        ListNode reversedHead = reverseLinkedList(listNode);
+
+        System.out.println("Развёрнутый список:");
+        printLinkedList(reversedHead);
+
+    }
+}
